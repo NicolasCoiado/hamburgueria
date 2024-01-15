f41d796c526a2c3f11a0ea3a7c36a060f9417468
Nicolas Coiado <nicolascoiado03@hotmail.com>
2024-01-15 11:48:06 -0300

    Adicionando vue-router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" id="logo" />
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/">About</router-link>
+
+    </nav>
+</template>
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+<style scoped></style>
